0b5306f4b587603e6563e21695a126c36faecb1f
tushar1597 <tusharsibal1597@gmail.com>
2020-11-23 14:50:11 +0530

    build-2

---
+++ build/precache-manifest.77ee6eec329d3cdf16d565df54248a9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c188a27c60bf12d90c44e44720a85d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bac4cf19670154c22af",
+    "url": "/static/css/main.a59bff8f.chunk.css"
+  },
+  {
+    "revision": "67dfd96396c9b6b23d5c",
+    "url": "/static/js/2.c36f8870.chunk.js"
+  },
+  {
+    "revision": "b3c52faaf5a3fc385823d249f4e2ce3d",
+    "url": "/static/js/2.c36f8870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bac4cf19670154c22af",
+    "url": "/static/js/main.6688ae0d.chunk.js"
+  },
+  {
+    "revision": "c4a40f422f070d498684",
+    "url": "/static/js/runtime-main.691a64da.js"
+  },
+  {
+    "revision": "0b0baeb13a319a86d41b9efaa72c2848",
+    "url": "/static/media/google-play.0b0baeb1.png"
+  },
+  {
+    "revision": "e77983234e2a9957fe8f81713a6e4c98",
+    "url": "/static/media/ust-logo.e7798323.png"
+  }
+]);
